0bf864e744a23bb68d99ab8cea6f9ba4a907c440
Jacob Manley <jbmanley45@gmail.com>
2022-02-15 12:32:04 -0800

    commit tree 672468bdc1d1dc708c89cef1a5b493d582374686

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(fill1="roller-skate") {
+   return(`This Saturday, I want to ${fill1}!`);
+}
+function mondayWork(fill2="go to the office") {
+    return(`This Monday, I will ${fill2}.`);
+}
+function wrapAdjective(flair="*"){
+    return function (adj="special") {
+        return `You are ${flair}${adj}${flair}!`;
+    }
+        
+   
+}
